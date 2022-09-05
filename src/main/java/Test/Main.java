9a5da40435f300d3a37b3e3806a4e5d68159fe21
Nurbolot <ngulamidinov@gmail.com>
2022-09-06 03:48:29 +0600

    Test updated - fix bugs

--- src/main/java/Test/Main.java
+++ src/main/java/Test/Main.java
@@ -4,6 +4,8 @@ package Test;
  * @version 1.0.0
  */
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
@@ -49,25 +51,104 @@ public class Main {
      */
 
     private static String romanCalc(String[] userInput) throws Exception {
-        RomanNumberEnum romanNumberEnum1 = RomanNumberEnum.valueOf(userInput[0]);
-        RomanNumberEnum romanNumberEnum2 = RomanNumberEnum.valueOf(userInput[2]);
-        int res = Integer.parseInt(arabicCalc(userInput[1], romanNumberEnum1.getTranslation(), romanNumberEnum2.getTranslation()));
+        int res = Integer.parseInt(arabicCalc(userInput[1], romanToInt(userInput[0]), romanToInt(userInput[2])));
         if (res <= 0) {
             throw new Exception("в римской системе нет 0 и отрицательных чисел");
         }
-        for (RomanNumberEnum rm : RomanNumberEnum.values()) {
-            if (rm.getTranslation() == res) {
-                return rm.toString();
-            }
+        return IntegerToRomanNumeral(res);
+    }
+
+    private static String IntegerToRomanNumeral(int input) {
+        if (input < 1 || input > 3999)
+            return "Invalid Roman Number Value";
+        String s = "";
+        while (input >= 1000) {
+            s += "M";
+            input -= 1000;        }
+        while (input >= 900) {
+            s += "CM";
+            input -= 900;
         }
-        return "";
+        while (input >= 500) {
+            s += "D";
+            input -= 500;
+        }
+        while (input >= 400) {
+            s += "CD";
+            input -= 400;
+        }
+        while (input >= 100) {
+            s += "C";
+            input -= 100;
+        }
+        while (input >= 90) {
+            s += "XC";
+            input -= 90;
+        }
+        while (input >= 50) {
+            s += "L";
+            input -= 50;
+        }
+        while (input >= 40) {
+            s += "XL";
+            input -= 40;
+        }
+        while (input >= 10) {
+            s += "X";
+            input -= 10;
+        }
+        while (input >= 9) {
+            s += "IX";
+            input -= 9;
+        }
+        while (input >= 5) {
+            s += "V";
+            input -= 5;
+        }
+        while (input >= 4) {
+            s += "IV";
+            input -= 4;
+        }
+        while (input >= 1) {
+            s += "I";
+            input -= 1;
+        }
+        return s;
+    }
+
+    private static int romanToInt(String s) {
+        Map<String, Integer> map = new HashMap<String, Integer>();
+        map.put("I", 1);
+        map.put("V", 5);
+        map.put("X", 10);
+        map.put("L", 50);
+        map.put("C", 100);
+        map.put("D", 500);
+        map.put("M", 1000);
+
+        int output = 0;
+        if (s.length() == 0) return 0;
+        if (s.length() == 1) return map.get(String.valueOf(s.charAt(0)));
+
+        for (int i = 0; i < (s.length() - 1); i++) {
+            if (map.get(String.valueOf(s.charAt(i))) >=
+                    map.get(String.valueOf(s.charAt(i + 1)))) {
+                output = output + map.get(String.valueOf(s.charAt(i)));
+            } else {
+                output = output - map.get(String.valueOf(s.charAt(i)));
+            }
+
+        }
+        output = output + map.get(String.valueOf(s.charAt(s.length() - 1)));
+        return output;
     }
 
     /**
-     *  выполнять операции сложения, вычитания, умножения и деления с двумя числами: a и b
+     * выполнять операции сложения, вычитания, умножения и деления с двумя числами: a и b
+     *
      * @param arithmeticSymbol - арифметическая операция
-     * @param num1 - первое число
-     * @param num2 - второе число
+     * @param num1             - первое число
+     * @param num2             - второе число
      * @return результат арифметической операции в строковом виде
      */
     private static String arabicCalc(String arithmeticSymbol, int num1, int num2) {
@@ -99,11 +180,11 @@ public class Main {
     }
 
     private static boolean isRomanNumber(String str) {
-        try {
-            RomanNumberEnum romanNumberEnum = RomanNumberEnum.valueOf(str);
-            return true;
-        } catch (Exception e) {
-            return false;
+        for (int i = 1; i <= 101; i++) {
+            if (str.equalsIgnoreCase(IntegerToRomanNumeral(i))) {
+                return true;
+            }
         }
+        return false;
     }
 }
